863c33119be3b6c83c798761164fd90a79861122
Adhi-dot <adhinugraha2228@gmail.com>
2026-04-09 10:36:18 +0700

    Create Codding matematika diskrit.py

---
+++ Codding matematika diskrit.py
@@ -0,0 +1,36 @@
+input_username = str(input("Masukkin username bro"))
+input_Password = str(input("Jangan lupa PW juga"))
+
+if input_username and input_Password:
+    if input_username == "The1onlydhi" :
+        if input_Password == "mabainformatika2026" :
+            print ("Welcome in man")
+        else :
+            print ("PW salah bro")
+    else :
+        print ("Username sama password salah bro")
+
+elif input_username :
+    if input_username =="The1onlydhi" :
+        print ("Masukkin PW lu bro")
+    else :
+        print("Username salah, PW ngga diisi")
+
+elif input_Password :
+    if input_Password == "mabainformatika2026" :
+        print ("Masukkin username bro")
+    else :
+        print ("PW salah, username ngga diisi")
+
+
+
+# The1onlydhi + mabainformatika2026 = TRUE
+# The1onlydhi + F = TRUE
+# F + mabainformatika2026 = TRUE
+# F + F = TRUE
+
+# The1onlydhi = TRUE
+# F +  = TRUE
+
+# mabainformatika2026 = TRUE
+#   + F = TRUE
